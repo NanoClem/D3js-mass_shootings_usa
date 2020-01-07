7806a1421bd2d2739f7100261d5c30dfdc74a41e
NanoClem <clemdecoop.pro@gmail.com>
2020-01-07 20:12:18 +0100

    implementing donut chart

---
+++ public/js/vis_donut.js
@@ -0,0 +1,161 @@
+var svg = d3.select("body")
+	.append("svg")
+	.append("g");
+
+svg.append("g")
+	.attr("class", "slices");
+svg.append("g")
+	.attr("class", "labels");
+svg.append("g")
+	.attr("class", "lines");
+
+var width = 960,
+    height = 450,
+	radius = Math.min(width, height) / 2;
+
+var pie = d3.layout.pie()
+	.sort(null)
+	.value(function(d) {
+		return d.value;
+	});
+
+var arc = d3.svg.arc()
+	.outerRadius(radius * 0.8)
+	.innerRadius(radius * 0.4);
+
+var outerArc = d3.svg.arc()
+	.innerRadius(radius * 0.9)
+	.outerRadius(radius * 0.9);
+
+svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+var key = function(d){ return d.data.label; };
+
+
+// IMPORT DATA
+d3.dsv(';')("datasets/mass-shootings-in-america.csv", function(data) {
+	
+	// ALL TYPES OF PLACES WHERE SHOOTINGS HAPPENED
+	var domains = d3.map(data, function(d) {return d.Place_Type;}).keys();
+
+	// COUNT OCCURRENCES IN PLACES
+	var countPlaces = {};
+	data.forEach( function(d) {
+		var place = d.Place_Type;
+		if(countPlaces[place] === undefined) {
+			countPlaces[place] = 0;
+		} else {
+			countPlaces[place] += 1;
+		}
+	});
+
+	// COLORS
+	var color = d3.scale.ordinal()
+		.domain(domains)
+		.range(["#7d0956", "#07adad", "#FF0000", "#07423d"]);
+
+
+
+	// GENERATE OUR DATA FOR THE CHART
+	function generateData() {
+		var labels = color.domain();
+		return labels.map(function(label){
+			return { label: label, value: countPlaces[label] }
+		});
+	}
+	
+
+	change(generateData());
+
+
+	function change(data) {
+
+		/* ------- PIE SLICES -------*/
+		var slice = svg.select(".slices").selectAll("path.slice")
+			.data(pie(data), key);
+
+		slice.enter()
+			.insert("path")
+			.style("fill", function(d) { return color(d.data.label); })
+			.attr("class", "slice");
+
+		slice		
+			.transition().duration(1000)
+			.attrTween("d", function(d) {
+				this._current = this._current || d;
+				var interpolate = d3.interpolate(this._current, d);
+				this._current = interpolate(0);
+				return function(t) {
+					return arc(interpolate(t));
+				};
+			})
+
+		slice.exit()
+			.remove();
+
+		/* ------- TEXT LABELS -------*/
+
+		var text = svg.select(".labels").selectAll("text")
+			.data(pie(data), key);
+
+		text.enter()
+			.append("text")
+			.attr("dy", ".35em")
+			.text(function(d) {
+				return d.data.label;
+			});
+		
+		function midAngle(d){
+			return d.startAngle + (d.endAngle - d.startAngle)/2;
+		}
+
+		text.transition().duration(1000)
+			.attrTween("transform", function(d) {
+				this._current = this._current || d;
+				var interpolate = d3.interpolate(this._current, d);
+				this._current = interpolate(0);
+				return function(t) {
+					var d2 = interpolate(t);
+					var pos = outerArc.centroid(d2);
+					pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+					return "translate("+ pos +")";
+				};
+			})
+			.styleTween("text-anchor", function(d){
+				this._current = this._current || d;
+				var interpolate = d3.interpolate(this._current, d);
+				this._current = interpolate(0);
+				return function(t) {
+					var d2 = interpolate(t);
+					return midAngle(d2) < Math.PI ? "start":"end";
+				};
+			});
+
+		text.exit()
+			.remove();
+
+		/* ------- SLICE TO TEXT POLYLINES -------*/
+
+		var polyline = svg.select(".lines").selectAll("polyline")
+			.data(pie(data), key);
+		
+		polyline.enter()
+			.append("polyline");
+
+		polyline.transition().duration(1000)
+			.attrTween("points", function(d){
+				this._current = this._current || d;
+				var interpolate = d3.interpolate(this._current, d);
+				this._current = interpolate(0);
+				return function(t) {
+					var d2 = interpolate(t);
+					var pos = outerArc.centroid(d2);
+					pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+					return [arc.centroid(d2), outerArc.centroid(d2), pos];
+				};			
+			});
+		
+		polyline.exit()
+			.remove();
+	};
+});
