832a9667cd2c86a60f5bf6aa151be13239ab8525
NanoClem <clemdecoop.pro@gmail.com>
2020-01-15 02:05:51 +0100

    change some values

--- public/js/charts/vis_donut.js
+++ public/js/charts/vis_donut.js
@@ -18,58 +18,41 @@ svgDonut.append("g")
 	.attr("class", "lines");
 
 
-var pie = d3.layout.pie()
+let pie = d3.layout.pie()
 	.sort(null)
 	.value(function(d) {
-		return d.value;
+		return d.count;
 	});
 
-var arc = d3.svg.arc()
+let arc = d3.svg.arc()
 	.outerRadius(radius * 0.8)
 	.innerRadius(radius * 0.4);
 
-var outerArc = d3.svg.arc()
+let outerArc = d3.svg.arc()
 	.innerRadius(radius * 0.9)
 	.outerRadius(radius * 0.9);
 
-var key = function(d){ return d.data.label; };
+let key = function(d){ return d.data.label; };
 
 
 // IMPORT DATA
 d3.dsv(';')("datasets/mass-shootings-in-america.csv", function(data) {
 
 	// ALL TYPES OF PLACES WHERE SHOOTINGS HAPPENED
-	var domains = d3.map(data, function(d) {return d.Place_Type;}).keys();
+	let domains = getLabels();
 
-	// COUNT OCCURRENCES IN PLACES
-	var countPlaces = {};
-	data.forEach( function(d) {
-		var place = d.Place_Type;
-		if(countPlaces[place] === undefined) {
-			countPlaces[place] = 0;
-		} else {
-			countPlaces[place] += 1;
-		}
-	});
-
+	// COUNT OCCURRENCES IN PLACE CATEGORIES
+	let donutData = countOccurences(data);
+	// console.log(countPlaces);
 
 	// COLORS
-	var color = d3.scale.ordinal()
+	let color = d3.scale.ordinal()
 		.domain(domains)
-		.range(["#7d0956", "#07adad", "#FF0000", "#07423d"]);
-
-
-	// GENERATE OUR DATA FOR THE CHART
-	function generateData() {
-		var labels = color.domain();
-		return labels.map(function(label){
-			return { label: label, value: countPlaces[label] };
-		});
-	}
+		.range(["#390B06", "#6F332D", "#17193C", "#3A6B45", "#568A62"]);
 
 	// ASCENDING SORT
-	var gData = generateData().sort(function(a, b) {
-		return a.value - b.value;
+	let gData = donutData.sort(function(a, b) {
+		return a.count - b.count;
 	});
 		
 	console.log(gData);
@@ -79,7 +62,7 @@ d3.dsv(';')("datasets/mass-shootings-in-america.csv", function(data) {
 	function change(data) {
 
 		/* ------- PIE SLICES -------*/
-		var slice = svgDonut.select(".slices").selectAll("path.slice")
+		let slice = svgDonut.select(".slices").selectAll("path.slice")
 			.data(pie(data), key);
 
 		slice.enter()
@@ -90,7 +73,7 @@ d3.dsv(';')("datasets/mass-shootings-in-america.csv", function(data) {
 		slice.transition().duration(1000)
 			 .attrTween("d", function(d) {
 				this._current = this._current || d;
-				var interpolate = d3.interpolate(this._current, d);
+				let interpolate = d3.interpolate(this._current, d);
 				this._current = interpolate(0);
 				return function(t) {
 					return arc(interpolate(t));
@@ -102,7 +85,7 @@ d3.dsv(';')("datasets/mass-shootings-in-america.csv", function(data) {
 
 		/* ------- TEXT LABELS -------*/
 
-		var text = svgDonut.select(".labels").selectAll("text")
+		let text = svgDonut.select(".labels").selectAll("text")
 			.data(pie(data), key);
 
 		text.enter()
@@ -119,21 +102,21 @@ d3.dsv(';')("datasets/mass-shootings-in-america.csv", function(data) {
 		text.transition().duration(1000)
 			.attrTween("transform", function(d) {
 				this._current = this._current || d;
-				var interpolate = d3.interpolate(this._current, d);
+				let interpolate = d3.interpolate(this._current, d);
 				this._current = interpolate(0);
 				return function(t) {
-					var d2 = interpolate(t);
-					var pos = outerArc.centroid(d2);
+					let d2 = interpolate(t);
+					let pos = outerArc.centroid(d2);
 					pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
 					return "translate("+ pos +")";
 				};
 			})
 			.styleTween("text-anchor", function(d) {
 				this._current = this._current || d;
-				var interpolate = d3.interpolate(this._current, d);
+				let interpolate = d3.interpolate(this._current, d);
 				this._current = interpolate(0);
 				return function(t) {
-					var d2 = interpolate(t);
+					let d2 = interpolate(t);
 					return midAngle(d2) < Math.PI ? "start":"end";
 				};
 			});
@@ -143,7 +126,7 @@ d3.dsv(';')("datasets/mass-shootings-in-america.csv", function(data) {
 
 		/* ------- SLICE TO TEXT POLYLINES -------*/
 
-		var polyline = svgDonut.select(".lines").selectAll("polyline")
+		let polyline = svgDonut.select(".lines").selectAll("polyline")
 			.data(pie(data), key);
 		
 		polyline.enter()
@@ -152,11 +135,11 @@ d3.dsv(';')("datasets/mass-shootings-in-america.csv", function(data) {
 		polyline.transition().duration(1000)
 			.attrTween("points", function(d) {
 				this._current = this._current || d;
-				var interpolate = d3.interpolate(this._current, d);
+				let interpolate = d3.interpolate(this._current, d);
 				this._current = interpolate(0);
 				return function(t) {
-					var d2 = interpolate(t);
-					var pos = outerArc.centroid(d2);
+					let d2 = interpolate(t);
+					let pos = outerArc.centroid(d2);
 					pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
 					return [arc.centroid(d2), outerArc.centroid(d2), pos];
 				};			
